f1e7d29d35d108390d0f8d071c014de393631dbf
lucasveron <veron_lucas@hotmail.com>
2022-09-07 16:19:48 -0300

    app node de prueba

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Payments
 Node Payment Service
+# Dependencias
+$ node -v
+>= v14.13.1
+$ docker -v
+>= Docker version 18.09.7
+# Levantar el servicio con Docker
+En la raíz del proyecto y con los permisos Docker necesarios correr:
+
+- `docker build -t ms-payments:0.0.1` genera la imagen docker
+- `docker run -d -p 3010:3010 -name ms-payments ms-payments:0.0.1` levanta la imagen anteriormente generada
+
+# Uso del API
+Hacer GET simple que retorna sin más
+`curl -X GET http://localhost:3010/`
---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const request = require("request");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const app = express();
+
+const PORT = 3010;
+const TIMEOUT = 5000;
+
+const id = Math.floor(Math.random() * 100);
+
+app.get("/", (req, res) => {
+  res.status(200).send("OK, I'm Up!\n");
+});
+
+app.get("/ping", (req, res) => {
+  console.log("ping OK\n");
+  res.status(200).send("OK, ping!\n");
+});
+
+app.get("/timeout", async (req, res) => {
+  console.log("comenzo timeout... ");
+  await new Promise((r) => setTimeout(r, 5000));
+  console.log("timeout termino! ");
+  res.status(200).send("OK, timeout 5 segundos\n");
+});
+
+app.get("/timeout-async", async (req, res) => {
+  this.timeoutObj;
+  console.log("timeout-async termino!");
+  res.status(200).send("OK, timeout-async 5 segundos\n");
+});
+
+app.get("/heavy", async (req, res) => {
+  var limit = 100000000;
+  var sum = 0;
+  var pi = 0;
+
+  let start_time = new Date();
+  console.log("Init time: " + start_time);
+
+  for (var i = 1; i <= limit; i++) sum += 1 / i ** 2;
+
+  pi = Math.sqrt(sum * 6);
+
+  let end_time = new Date();
+  console.log("End time: " + end_time);
+  console.log("Diff time: " + Math.abs(start_time - end_time) / 1000 + "s");
+
+  res.status(200).send(id + " - PI: " + pi + "\n");
+});
+
+app.listen(PORT, () => {
+  console.log("Escuchando en puerto", PORT);
+});
